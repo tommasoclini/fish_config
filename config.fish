dd5ba557a654eea2bebf2fd713db5da77d496f80
Tommaso Clini <tommaso@localhost.localdomain>
2024-12-05 16:37:32 +0100

    fish_variables should not be in config

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+    fish_greeting
 end
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
